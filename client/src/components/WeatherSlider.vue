c52d516f0032951c37f173154d286e41fad93407
neuwelt <ineuwelt@neuwelt.co>
2025-05-28 14:25:56 +0200

    remove empty line

---
+++ client/src/components/WeatherSlider.vue
@@ -0,0 +1,16 @@
+<!-- filepath: /home/isaac/projects/weather-dashboard/client/src/components/WeatherSlider.vue -->
+<template>
+  <div>
+    <h2>Weather Slider Component</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WeatherSlider',
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
